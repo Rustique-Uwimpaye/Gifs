8ccf3899d1b0d2c37205b5de64b6bee3c1922736
Rustique-Uwimpaye <rustique644@gmail.com>
2020-10-01 11:40:31 +0300

    added header, gifs, search and css

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  gifsApiKey:'0oi1y1cs45MgbrBR4hPhTpPX1I19mLNg'
 };
 
 /*
